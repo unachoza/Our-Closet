18e3aa25055ef9fe710115c3d225ca4effbbff10
Arianna Choza <unachoza@gmail.com>
2023-10-23 23:45:29 -0400

    outline

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>Our Closet</h1>
+      <div>
+      <h2>Select User</h2>
+      </div>
+      <div>
+        <h2>Select Wardrobe</h2>
+        <h2>Select Season</h2>
+        <h2>Select Size</h2>
+        <h2>Select Color</h2>
+         <h2>Select Brand</h2>
+      </div>
     </div>
   );
 }
